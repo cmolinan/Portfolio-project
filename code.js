8f997c5c505edd00085f1249d56a534d5dd6335f
cmolinan <cmolinan@gmail.com>
2022-07-19 11:19:02 -0500

    Erase comments in styles.css and update to code.js

--- code.js
+++ code.js
@@ -1,10 +1,14 @@
+const queryMobileMenu = document.getElementById('MobileMenu');
+const queryMenuIcon = document.getElementById ('menu-icon');
+const queryCloseMenu = document.getElementById ('close-menu');
+
 function MobileMenu () {
-  if (document.getElementById('MobileMenu').style.display == 'block') {
-    document.getElementById('MobileMenu').style.display = 'none';
+  if ( queryMobileMenu.style.display == 'block') {
+    queryMobileMenu.style.display = 'none';
   } else {
-     document.getElementById('MobileMenu').style.display = 'block';
+    queryMobileMenu.style.display  = 'block';
     }
 }
-  
-document.getElementById ('menu-icon').addEventListener('click', MobileMenu);
-document.getElementById ('close-menu').addEventListener('click', MobileMenu);
+
+queryMenuIcon.addEventListener('click', MobileMenu);
+queryCloseMenu.addEventListener('click', MobileMenu);
