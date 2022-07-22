74df7927a001e0ff61218580d2f5408fab2a8ef7
cmolinan <cmolinan@gmail.com>
2022-07-22 16:21:34 -0500

    Adding preservation of form data in the browser

--- cards.js
+++ cards.js
@@ -195,5 +195,32 @@ function createPopupSection() {
   )
 }
 
+//For the Popup Cards
+function WorkCards (popcardNumber) {
+
+  if ( document.getElementById(popcardNumber).style.display == 'block') {
+   document.getElementById(popcardNumber).style.display = 'none';    
+   document.getElementById('popup-work-section').style.display = 'none';
+   document.getElementById('toolbar').style.display = 'flex';
+   document.getElementById('section-headline').style.display = 'block';
+   document.getElementById('work-section').style.display = 'grid';
+   
+   document.getElementById('about-section').style.display = 'flex';
+   document.getElementById('contact-background').style.display = 'block'; 
+
+   //jump to the original Card 
+   window.location.href = "#work"+ popcardNumber.substring(7, 8);
+  } else {
+   document.getElementById(popcardNumber).style.display = 'block';
+   document.getElementById('popup-work-section').style.display = 'flex';
+   document.getElementById('toolbar').style.display = 'none';
+   document.getElementById('section-headline').style.display = 'none';
+   document.getElementById('work-section').style.display = 'none';
+   
+   document.getElementById('about-section').style.display = 'none';
+   document.getElementById('contact-background').style.display = 'none';    
+ }
+}
+
 createWorkSection();
 createPopupSection();
